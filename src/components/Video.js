c89584607743fca00bed4bd4cf23585174fabb3d
Stepan <ssavdeyuk@edu.hse.ru>
2023-03-14 18:32:53 +0300

    bg fix

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
 import video from "../assets/background.mp4"
-// import videoSafari from "../assets/background-min.gif"
+import videoSafari from "../assets/background.gif"
 
 const Video = () => {
 
-  // const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+  const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
   return (
     <div className="video__wrapper">
-      {/* {isSafari && <video src={videoSafari} autoPlay muted loop/>} */}
-      {/* {!isSafari && <video src={video} autoPlay muted loop/>} */}
-      <video src={video} autoPlay muted loop/>
+      {isSafari && <img src={videoSafari}/>}
+      {!isSafari && <video src={video} autoPlay muted loop/>}
     </div>
   )
 }
