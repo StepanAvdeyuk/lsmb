3134278ad97c37d85500417618b26fb49328a4a6
Stepan <ssavdeyuk@edu.hse.ru>
2023-03-14 17:35:44 +0300

    bug fix

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,7 +5,7 @@ import video from "../assets/background.mp4"
 const Video = () => {
   return (
     <div className="video__wrapper">
-      <video src={video} autoPlay muted loop/>
+      <video src={video} autoPlay muted playsinline loop/>
     </div>
   )
 }
