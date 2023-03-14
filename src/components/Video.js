f837f5b3f4da0ec86a42cdfca9bb1b44d18370a7
Stepan <ssavdeyuk@edu.hse.ru>
2023-03-14 17:53:53 +0300

    bug fix

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 import video from "../assets/background.mp4"
+import videoSafari from "../assets/background-min.gif"
 
 const Video = () => {
+
+  const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
   return (
     <div className="video__wrapper">
-      <video src={video} autoPlay muted playsinline loop/>
+      {isSafari && <video src={videoSafari} autoPlay muted loop/>}
+      {!isSafari && <video src={video} autoPlay muted loop/>}
     </div>
   )
 }
