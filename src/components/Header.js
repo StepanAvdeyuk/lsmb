0f65c8d3517a3dec2d22d96d41ab9dc2b6be5251
Stepan <ssavdeyuk@edu.hse.ru>
2023-03-14 20:35:19 +0300

    bug fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,32 +13,34 @@ const Header = () => {
 
   return (
     <>
-    <div className="container">
-        <div className="header__wrapper" id="header">
-            <div className="header__left">
-                <img className='logo' src={logo} alt="logo" />
-                <nav>
-                    <ul>
-                        <Link to="register" smooth={true} duration={400} offset={-200} onClick={() => navigate("/")}><li>Регистрация</li></Link>
-                        <Link to="buy" smooth={true} duration={600} offset={-80} onClick={() => navigate("/")}><li>Купить билеты</li></Link>
-                        <Link to="footer" smooth={true} duration={700} offset={-80} onClick={() => navigate("/")}><li>Контакты</li></Link>
-                    </ul>
-                </nav>
-            </div>
-            <div className="header__burger" onClick={() => setIsOpenSidebar(true)}>
-                <div></div>
-                <div></div>
-                <div></div>
+    <div className="header__container">
+        <div className="container">
+            <div className="header__wrapper" id="header">
+                <div className="header__left">
+                    <img className='logo' src={logo} alt="logo" />
+                    <nav>
+                        <ul>
+                            <Link to="register" smooth={true} duration={400} offset={-200} onClick={() => navigate("/")}><li>Регистрация</li></Link>
+                            <Link to="buy" smooth={true} duration={600} offset={-130} onClick={() => navigate("/")}><li>Купить билеты</li></Link>
+                            <Link to="footer" smooth={true} duration={700} offset={-80} onClick={() => navigate("/")}><li>Контакты</li></Link>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="header__burger" onClick={() => setIsOpenSidebar(true)}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
         </div>
     </div>
     <div className={isOpenSidebar ? "header__sidebar active" : "header__sidebar"} >
-        <div className="header__sidebar-close" onClick={() => setIsOpenSidebar(false)}>✕</div>
-        <ul>
-            <Link to="register" smooth={true} duration={400} offset={-200} onClick={() => {setIsOpenSidebar(false); navigate('/')}}><li>Регистрация</li></Link>
-            <Link to="buy" smooth={true} duration={600} offset={-80} onClick={() => {setIsOpenSidebar(false); navigate('/')}}><li>Купить билеты</li></Link>
-            <Link to="footer" smooth={true} duration={700} offset={-80} onClick={() => {setIsOpenSidebar(false); navigate('/')}}><li>Контакты</li></Link>
-        </ul>
+            <div className="header__sidebar-close" onClick={() => setIsOpenSidebar(false)}>✕</div>
+            <ul>
+                <Link to="register" smooth={true} duration={400} offset={-200} onClick={() => {setIsOpenSidebar(false); navigate('/')}}><li>Регистрация</li></Link>
+                <Link to="buy" smooth={true} duration={600} offset={-130} onClick={() => {setIsOpenSidebar(false); navigate('/')}}><li>Купить билеты</li></Link>
+                <Link to="footer" smooth={true} duration={700} offset={-80} onClick={() => {setIsOpenSidebar(false); navigate('/')}}><li>Контакты</li></Link>
+            </ul>
     </div>
     </>
   )
